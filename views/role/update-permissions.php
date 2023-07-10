440fc10b9eae1834efc1b5b73cca0b6ef743aa41
Ipsita Soumya <ipsitasoumya1818@gmail.com>
2023-07-10 17:10:07 +0530

    Update permissions

--- views/role/update-permissions.php
+++ views/role/update-permissions.php
@@ -14,25 +14,25 @@ $this->params['breadcrumbs'][] = 'Update Permissions';
 <?php $form = ActiveForm::begin(['method' => 'post']); ?>
 
 <h3>User Stories</h3>
-<?= $form->field($role, 'permission_view_user_story')->checkbox() ?>
-<?= $form->field($role, 'permission_add_user_story')->checkbox() ?>
-<?= $form->field($role, 'permission_modify_user_story')->checkbox() ?>
-<?= $form->field($role, 'permission_comment_user_story')->checkbox() ?>
-<?= $form->field($role, 'permission_delete_user_story')->checkbox() ?>
+<?= $form->field($role, 'permission_view_user_story')->checkbox(['checked' => $role->permission_view_user_story ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_add_user_story')->checkbox(['checked' => $role->permission_add_user_story ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_modify_user_story')->checkbox(['checked' => $role->permission_modify_user_story ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_comment_user_story')->checkbox(['checked' => $role->permission_comment_user_story ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_delete_user_story')->checkbox(['checked' => $role->permission_delete_user_story ? 'checked' : '']) ?>
 
 <h3>Tasks</h3>
-<?= $form->field($role, 'permission_view_tasks')->checkbox() ?>
-<?= $form->field($role, 'permission_add_tasks')->checkbox() ?>
-<?= $form->field($role, 'permission_modify_tasks')->checkbox() ?>
-<?= $form->field($role, 'permission_comment_tasks')->checkbox() ?>
-<?= $form->field($role, 'permission_delete_tasks')->checkbox() ?>
+<?= $form->field($role, 'permission_view_tasks')->checkbox(['checked' => $role->permission_view_tasks ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_add_tasks')->checkbox(['checked' => $role->permission_add_tasks ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_modify_tasks')->checkbox(['checked' => $role->permission_modify_tasks ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_comment_tasks')->checkbox(['checked' => $role->permission_comment_tasks ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_delete_tasks')->checkbox(['checked' => $role->permission_delete_tasks ? 'checked' : '']) ?>
 
 <h3>Issues</h3>
-<?= $form->field($role, 'permission_view_issues')->checkbox() ?>
-<?= $form->field($role, 'permission_add_issues')->checkbox() ?>
-<?= $form->field($role, 'permission_modify_issues')->checkbox() ?>
-<?= $form->field($role, 'permission_comment_issues')->checkbox() ?>
-<?= $form->field($role, 'permission_delete_issues')->checkbox() ?>
+<?= $form->field($role, 'permission_view_issues')->checkbox(['checked' => $role->permission_view_issues ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_add_issues')->checkbox(['checked' => $role->permission_add_issues ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_modify_issues')->checkbox(['checked' => $role->permission_modify_issues ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_comment_issues')->checkbox(['checked' => $role->permission_comment_issues ? 'checked' : '']) ?>
+<?= $form->field($role, 'permission_delete_issues')->checkbox(['checked' => $role->permission_delete_issues ? 'checked' : '']) ?>
 <div class="form-group">
     <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
 </div>
